7e74c9d554563ebb1f275f75e6837e8952a21fb1
valfranciscangeli <vvfranciscangeli@gmail.com>
2024-10-06 18:48:40 -0300

    avance

---
+++ 20240913164939C39DC679072132__base.hs
@@ -0,0 +1,122 @@
+module Main where
+
+import Prelude hiding (cycle)
+import Control.Monad.State
+import Numeric.Probability.Distribution hiding (map,coin,filter)
+
+
+
+{-------------------------------------------}
+{--------------  EJERCICIO 1  --------------}
+{-------------------------------------------}
+
+-- First component: # moves to the right
+-- Second component: # moves up
+type Pos = (Int,Int)
+
+-- Parte (a)
+possibleMoves :: Pos -> [Pos]
+possibleMoves = undefined
+-- complete la definicion
+
+-- Parte (b)
+nSteps :: Int -> Pos -> [Pos]
+nSteps = undefined
+-- complete la definicion
+
+
+
+
+{-------------------------------------------}
+{--------------  EJERCICIO 2  --------------}
+{-------------------------------------------}
+
+data Result e g = Fail e | Good g
+
+instance Functor (Result e) where
+  -- fmap :: (a->b) -> Result e a -> Result e b
+  fmap f (Fail err) = Fail err
+  fmap f (Good res) = Good (f res)
+
+-- Parte (a)
+
+
+
+
+-- Parte (b)
+type Shop = String
+
+cycle :: Shop -> Shop -> Result String ()
+cycle = error "cycle undef"
+
+shop :: Shop -> Result String ()
+shop = error "shop unfed"
+
+doGroceries :: [Shop] -> Result String ()
+doGroceries []         = Fail "No Shops to visit!"
+doGroceries [x]        = shop x
+doGroceries (x1:x2:xs) = case shop x1 of
+    Fail e1 -> Fail e1
+    Good _  -> case cycle x1 x2 of
+        Fail e2 -> Fail e2
+        Good _  -> doGroceries (x2:xs)
+
+
+
+
+{-------------------------------------------}
+{--------------  EJERCICIO 3  --------------}
+{-------------------------------------------}
+
+data Command = Inc | Dec | Mult | Store
+
+-- Primer componente: numero en la hoja
+-- Segundo componente: numero en la mano de Bob
+type GameState = (Int,Int)
+
+playGame :: [Command] -> State GameState Int
+playGame = undefined
+-- complete la definicion
+
+
+
+
+{-------------------------------------------}
+{--------------  EJERCICIO 4  --------------}
+{-------------------------------------------}
+
+type Probability = Rational
+type Dist a = T Probability a
+
+
+-- Parte (a.I)
+pointDist :: Int -> Dist (Int, Int)
+pointDist = undefined
+-- complete la definicion
+
+-- Parte (a.II)
+resultE3a :: Int -> Probability
+resultE3a = undefined
+-- complete la definicion
+
+
+-- Parte (b)
+{-
+Si le resulta conveniente, puede empezar siguiendo
+el hint dado:
+
+data Uni     = Chile | Cato deriving Eq
+type Urn     = (Int, Int)
+-- 1er componente: #jugadores Chile, 2do componente: #jugadores Cato
+
+pickPlayer :: Urn -> Dist (Uni, Urn)
+pickPlayer = undefined
+-}
+
+
+resultE3b :: Probability
+resultE3b = undefined
+-- complete la definicion
+
+main :: IO()
+main = return ()
--- app/Main.hs
+++ app/Main.hs
@@ -1,38 +1,56 @@
+import Data.List (nub)
 import T3
+import Test.Hspec
+
 -- Pruebas para EJERCICIO 1 =============================
 
 -- Parte (a):------
 testsEjecicio1ParteA :: Spec
--- testsEjecicio1ParteA = describe "EJERCICIO 1.a) posibleMoves" $ do
---   it "pruebas para posibleMoves" $ do
---     evalCF (Simple 5) `shouldBe` (5, 1)
+testsEjecicio1ParteA =
+  describe "EJERCICIO 1.a) possibleMoves" $ do
+    it "prueba en (3, 0)" $ do
+      possibleMoves (3, 0) `shouldBe` [(3, 1)] -- solo puede subir
+    it "prueba en (0, 3)" $ do
+      possibleMoves (0, 3) `shouldBe` [(1, 3)] -- solo puede ir a la derecha
+    it "prueba en (0,0)" $ do
+      possibleMoves (0, 0) `shouldBe` [(1, 0), (0, 1)]
+    it "prueba en (2, 1)" $ do
+      possibleMoves (2, 1) `shouldBe` [(3, 1), (2, 2)]
+    it "prueba en (2, 2)" $ do
+      possibleMoves (2, 2) `shouldBe` [(3, 2), (2, 3)]
 
 -- Parte (b):------
 testsEjecicio1ParteB :: Spec
--- testsEjecicio1ParteB = describe "EJERCICIO 1.b) nSteps" $ do
---   it "pruebas para posibleMoves" $ do
---     evalCF (Simple 5) `shouldBe` (5, 1)
-
+testsEjecicio1ParteB = describe "EJERCICIO 1.b) nSteps" $ do
+  it "nSteps 0 (0,0)" $ do
+    nSteps 0 (0, 0) `shouldBe` [(0, 0)]
+  it "nSteps 1 (0,0)" $ do
+    nSteps 1 (0, 0) `shouldBe` [(1, 0), (0, 1)]
+  it "nSteps 2 (0,0)" $ do
+    nSteps 2 (0, 0) `shouldBe` [(2, 0), (1, 1), (0, 2)]
+  it "nSteps 3 (0,0)" $ do
+    nSteps 3 (0, 0) `shouldBe` [(3, 0), (2, 1), (1, 2), (0, 3)]
+  it "nSteps 2 (1,1)" $ do
+    nSteps 2 (1, 1) `shouldBe` [(3, 1), (2, 2), (1, 3)]
 
 -- Pruebas para EJERCICIO 2 =============================
 
 -- Parte (a):------
-testsEjecicio2ParteA :: Spec
+-- testsEjecicio2ParteA :: Spec
 -- testsEjecicio2ParteA = describe "EJERCICIO 2.a) posibleMoves" $ do
 --   it "pruebas para posibleMoves" $ do
 --     evalCF (Simple 5) `shouldBe` (5, 1)
 
 -- Parte (b):------
-testsEjecicio2ParteB :: Spec
+-- testsEjecicio2ParteB :: Spec
 -- testsEjecicio2ParteB = describe "EJERCICIO 2.b) nSteps" $ do
 --   it "pruebas para posibleMoves" $ do
 --     evalCF (Simple 5) `shouldBe` (5, 1)
-  
 
 -- Pruebas para EJERCICIO 3 =============================
 
 -- Ejercicio 3:------
-testsEjecicio3 :: Spec
+-- testsEjecicio3 :: Spec
 -- testsEjecicio3 = describe "EJERCICIO 3) posibleMoves" $ do
 --   it "pruebas para posibleMoves" $ do
 --     evalCF (Simple 5) `shouldBe` (5, 1)
@@ -40,13 +58,13 @@ testsEjecicio3 :: Spec
 -- Pruebas para EJERCICIO 4 =============================
 
 -- Parte (a):------
-testsEjecicio4ParteA :: Spec
+-- testsEjecicio4ParteA :: Spec
 -- testsEjecicio4ParteA = describe "EJERCICIO 4.a) posibleMoves" $ do
 --   it "pruebas para posibleMoves" $ do
 --     evalCF (Simple 5) `shouldBe` (5, 1)
 
 -- Parte (b):------
-testsEjecicio4ParteB :: Spec
+-- testsEjecicio4ParteB :: Spec
 -- testsEjecicio4ParteB = describe "EJERCICIO 4.b) nSteps" $ do
 --   it "pruebas para posibleMoves" $ do
 --     evalCF (Simple 5) `shouldBe` (5, 1)
@@ -60,11 +78,12 @@ main = hspec $ do
   -- Ejercicio 1
   testsEjecicio1ParteA
   testsEjecicio1ParteB
-  -- Ejercicio 2
-  testsEjecicio2ParteA
-  testsEjecicio2ParteB
-  -- Ejercicio 3
-  testsEjecicio3
-  -- Ejercicio 4
-  testsEjecicio4A
-  testsEjecicio4B
+
+-- -- Ejercicio 2
+-- testsEjecicio2ParteA
+-- testsEjecicio2ParteB
+-- -- Ejercicio 3
+-- testsEjecicio3
+-- -- Ejercicio 4
+-- testsEjecicio4A
+-- testsEjecicio4B
